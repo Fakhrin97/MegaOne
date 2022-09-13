792ea6bc2447b8a213932fb251d70a9a658c713d
Fakhrin97 <fakhrinya@code.edu.az>
2022-09-13 10:06:48 +0400

    Mega One

---
+++ shop/shop/Models/Prodacts.cs
@@ -0,0 +1,13 @@
+﻿namespace shop.Models
+{
+    public class Prodacts
+    {
+        public int Id { get; set; }
+
+        public string  Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
